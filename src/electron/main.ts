a52c25fb33d7b72ac3b570f1122a3bcb02849a2c
nyara <nyaranra.v@gmail.com>
2025-02-10 16:30:27 +0900

    [ref];の削除

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+    const mainWindow = new BrowserWindow({})
+    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'))
 })
