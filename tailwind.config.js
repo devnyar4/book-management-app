e9ca311c7043376d3b6d51c8911930020a16555c
nyara <nyaranra.v@gmail.com>
2025-02-10 16:29:39 +0900

    [update]カスタムカラーの追記

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shockingPink: '#ff007f',
+        dark: '1a1a1a',
+        lightGray: '#e5e5e5',
+      }
+    },
   },
   plugins: [],
 }
